5c7d4859e21ea9ffda139c61db0ec841e6c12f4f
Emilia La Grasta <emilialagrasta@alumnos.fi.mdp.edu.ar>
2026-03-31 19:34:26 -0300

    commit inicial

---
+++ SistemaGestionDeFilas/src/main/java/grupo1/sistemagestiondefilas/SistemaGestionDeFilas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grupo1.sistemagestiondefilas;
+
+/**
+ *
+ * @author emila
+ */
+public class SistemaGestionDeFilas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
