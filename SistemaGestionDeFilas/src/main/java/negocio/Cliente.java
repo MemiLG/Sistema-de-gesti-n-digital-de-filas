cf88f11e88f4e869006b87889c1e5063ee1c4c52
Emilia La Grasta <emilialagrasta@alumnos.fi.mdp.edu.ar>
2026-04-03 20:42:43 -0300

    Logica de cliente y cola ingreso

--- SistemaGestionDeFilas/src/main/java/negocio/Cliente.java
+++ SistemaGestionDeFilas/src/main/java/negocio/Cliente.java
@@ -2,5 +2,12 @@ package negocio;
 
 // aca va la logica del cliente
 public class Cliente {
+    private int DNI;
     
+    public int getDNI(){
+        return this.DNI;
+    }
+    public void setDNI(int dni){
+        this.DNI = dni;
+    }
 }
--- SistemaGestionDeFilas/src/main/java/negocio/ColaIngreso.java
+++ SistemaGestionDeFilas/src/main/java/negocio/ColaIngreso.java
@@ -1,6 +1,25 @@
 package negocio;
 
 // Aca va la logica
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class ColaIngreso {
+    private final Queue<Integer> colaIng = new LinkedList<>();
     
+    public Queue<Integer> getCola (){
+        return this.colaIng;
+    }
+    
+    public void nuevoIngreso(int dni){
+        colaIng.add(dni);
+    }
+    
+    public int sacarCliente(){
+        Integer elem = colaIng.poll();
+        if (elem == null){
+            elem = 0;
+        }
+        return elem;
+    }
 }
