9c750c01b13f9d26eba571a27d18a328b9b0361d
Emilia La Grasta <emilialagrasta@alumnos.fi.mdp.edu.ar>
2026-04-13 10:46:59 -0300

    Actualizacion Servidor

--- SistemaGestionDeFilas/src/main/java/servidor/Servidor.java
+++ SistemaGestionDeFilas/src/main/java/servidor/Servidor.java
@@ -6,6 +6,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import negocio.ColaIngreso;
 import negocio.Historial;
+
 public class Servidor {
     private ColaIngreso colaIng;
     private Historial historial;
