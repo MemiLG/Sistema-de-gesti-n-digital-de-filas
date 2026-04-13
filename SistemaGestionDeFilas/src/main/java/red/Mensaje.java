7128d0e3811489f30b818540a6ca6b999bb4a8e7
Emilia La Grasta <emilialagrasta@alumnos.fi.mdp.edu.ar>
2026-04-13 10:37:18 -0300

    Cambio en la arquitectura. Cominezo del armado del componente Servidor

--- SistemaGestionDeFilas/src/main/java/red/Mensaje.java
+++
@@ -1,7 +0,0 @@
-
-package red;
-
-// Este es un objeto serializable que viaja por la red
-public class Mensaje {
-    
-}
---
+++ SistemaGestionDeFilas/src/main/java/servidor/Servidor.java
@@ -0,0 +1,40 @@
+
+package servidor;
+//Aca van a estar las colas. Todos los componentes tienen que comunicarse con el servidor para tener la info que quieren
+
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import negocio.ColaIngreso;
+import negocio.Historial;
+public class Servidor {
+    private ColaIngreso colaIng;
+    private Historial historial;
+    private ServerSocket serverSocket;
+    private boolean escuchando = false;
+    private String ip = "";
+    private int puerto;
+    
+    public Servidor(){
+        colaIng = new ColaIngreso();
+        historial = new Historial();
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+
+        }
+    }
+    
+    public ColaIngreso getColaIng(){
+        return this.colaIng;
+    }
+    
+    public Historial getHistorial(){
+        return this.historial;
+    }
+    
+    public void setPuerto(int puertoNuevo){
+        this.puerto = puertoNuevo;
+    }
+    
+    
+}
